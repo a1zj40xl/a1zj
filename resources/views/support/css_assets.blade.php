0d7a7d772c04141b5347336381f8457ddb336af5
testoodoo1 <test.oodoo1@gmail.com>
2019-01-04 01:17:49 +0530

    blade - css - in progress

--- resources/views/support/css_assets.blade.php
+++ resources/views/support/css_assets.blade.php
@@ -1,3 +1,3 @@
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-  <link href="../assets/css/material-dashboard.min.css?v=2.1.0" rel="stylesheet" />
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+  {!! HTML::style( assets('../assets/css/font-awesome.min.css'))!!}
+  {!! HTML::style('../assets/css/material-dashboard.min.css?v=2.1.0')!!}
+  {!! HTML::style('../assets/demo/demo.css')!!}
