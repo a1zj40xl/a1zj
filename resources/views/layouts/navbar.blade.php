38542c16a89726f5a92e386c22c6079d1b144e66
testoodoo1 <test.oodoo1@gmail.com>
2019-01-04 01:04:01 +0530

    js & css assets file added

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,4 @@
-      <!-- Navbar -->
+
       <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
         <div class="container-fluid">
           <div class="navbar-wrapper">
---
+++ resources/views/support/css_assets.blade.php
@@ -0,0 +1,3 @@
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <link href="../assets/css/material-dashboard.min.css?v=2.1.0" rel="stylesheet" />
+  <link href="../assets/demo/demo.css" rel="stylesheet" />
