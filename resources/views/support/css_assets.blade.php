5a2ce022cb91d58d140d7abeb0b22eb1cd127725
testoodoo1 <test.oodoo1@gmail.com>
2019-01-30 18:33:14 +0530

    jaiganesh interview

--- resources/views/support/css_assets.blade.php
+++ resources/views/support/css_assets.blade.php
@@ -1,3 +1,9 @@
-  {!! HTML::style('./assets/css/font-awesome.min.css')!!}
-  {!! HTML::style('../assets/css/material-dashboard.min.css?v=2.1.0')!!}
-  {!! HTML::style('../assets/demo/demo.css')!!}
+  
+
+
+  {!! HTML::style('/assets/css/font-awesome.min.css') !!}
+  {{ HTML::style('/assets/dist/admin/css/bootstrap.min.css') }}
+  {{ HTML::style('/assets/css/material-dashboard.min.css?v=2.1.0') }}
+  {{ HTML::style('/assets/demo/demo.css') }}
+
+
