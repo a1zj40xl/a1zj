dd62a3e8da65a379b2ba0d9abdf1101ea01009c7
testoodoo1 <test.oodoo1@gmail.com>
2019-01-30 20:32:44 +0530

    inspect element error cleared (almost) & css added

---
+++ error.php
@@ -0,0 +1,15 @@
+not able to add HTML and FORM in laravel 5.7
+
+
+  {{ HTML::style('/assets/css/font-awesome.min.css') }}
+  {{ HTML::style('/assets/dist/admin/css/bootstrap.min.css') }}
+  {{ HTML::style('/assets/css/material-dashboard.min.css?v=2.1.0') }}
+  {{ HTML::style('/assets/demo/demo.css') }}
+
+ sol: proviver, composer and aliases added, but not working
+
+
+ <link href="{{ asset('css/font-awesome.min.css') }}" media="all" rel="stylesheet" type="text/css" />
+<link href="{{ asset('dist/admin/css/bootstrap.min.css') }}" media="all" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/material-dashboard.min.css?v=2.1.0') }}" media="all" rel="stylesheet" type="text/css" />
+<link href="{{ asset('demo/demo.css') }}" media="all" rel="stylesheet" type="text/css" />
--- resources/views/support/css_assets.blade.php
+++ resources/views/support/css_assets.blade.php
@@ -1,9 +1,12 @@
   
 
 
-  {!! HTML::style('/assets/css/font-awesome.min.css') !!}
-  {{ HTML::style('/assets/dist/admin/css/bootstrap.min.css') }}
-  {{ HTML::style('/assets/css/material-dashboard.min.css?v=2.1.0') }}
-  {{ HTML::style('/assets/demo/demo.css') }}
+
+
+
+<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="assets/css/material-dashboard.min.css?v=2.1.0" rel="stylesheet" />
+<link href="assets/demo/demo.css" rel="stylesheet" />
 
 
